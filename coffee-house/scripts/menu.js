8ca8647266555cceb30fe5fc38ffc80e838b723a
Maxim Alyoshin <maxim.alyosh@gmail.com>
2023-12-09 22:59:23 +0500

    feat: implement async func getProductsJSON in menu

--- coffee-house/scripts/menu.js
+++ coffee-house/scripts/menu.js
@@ -1,61 +1,205 @@
-const coffeeMenuLink = document.querySelector('.coffee-menu__link');
-const tabCoffeeBtn = document.querySelector('.tab-coffee');
-const tabTeaBtn = document.querySelector('.tab-tea');
-const tabDessertBtn = document.querySelector('.tab-dessert');
+'use strict';
+const coffeeMenuLink = document.querySelector(".coffee-menu__link");
+const tabCoffeeBtn = document.querySelector(".tab-coffee");
+const tabTeaBtn = document.querySelector(".tab-tea");
+const tabDessertBtn = document.querySelector(".tab-dessert");
 let tabInd = 0;
 const tabsBtn = [tabCoffeeBtn, tabTeaBtn, tabDessertBtn];
-const cupsCards = Array.from(document.querySelector('.menu__container').children);
-const closeButtons = Array.from(document.querySelectorAll('.close__btn'));
-const modalWindow = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const menuCardsFirst = Array.from(document.querySelectorAll('.cup-card__first'));
-const page = document.querySelector('html')
+const cupsCards = Array.from(document.querySelector(".menu__container").children);
+const closeButtons = Array.from(document.querySelectorAll(".close__btn"));
 
-tabCoffeeBtn.addEventListener('click', () => switchTabs(0));
-tabTeaBtn.addEventListener('click', () => switchTabs(1));
-tabDessertBtn.addEventListener('click', () => switchTabs(2));
+//Modal
+const modalWindow = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const menuCards = Array.from(document.querySelectorAll(".menu__cup-card"));
+const page = document.querySelector("html");
+const jsonURL = "https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/coffee-house/products.json";
+const rangeSections = {
+    coffee: {
+        start: 0,
+        end: 7,
+    },
+    tea: {
+        start: 8,
+        end: 11,
+    },
+    dessert: {
+        start: 12,
+        end: 19,
+    },
+};
+let products;
 
-for (let closeButton of closeButtons) {
-    closeButton.addEventListener('click', () => closeModal())
+
+async function getJSON() {
+    let response = await fetch(jsonURL);
+    products = await response.json(); // читаем ответ в формате JSON
+//     console.log(products);
 }
 
-for (let cardFirst of menuCardsFirst) {
-    cardFirst.addEventListener('click', () => openModal())
+
+
+// let request = new XMLHttpRequest();
+// request.open("GET", jsonURL);
+// request.responseType = "json";
+// request.send();
+//
+//
+// if (request.status != 200) {
+//   console.log( request.status + ': ' + request.statusText + ' данные не пришли');
+// } else {
+//   console.log('ok')
+//   names = xhr.responseText;
+//   request.onload = function () {
+//       products = request.response;
+//   };
+// }
+
+tabCoffeeBtn.addEventListener("click", () => switchTabs(0));
+tabTeaBtn.addEventListener("click", () => switchTabs(1));
+tabDessertBtn.addEventListener("click", () => switchTabs(2));
+
+for (let closeButton of closeButtons) {
+    closeButton.addEventListener("click", () => closeModal());
+}
+
+for (let cardFirst of menuCards) {
+    cardFirst.addEventListener("click", () => openModal());
 }
 
 function showUnderline() {
     let currentUrl = window.location.href;
-    if (currentUrl.split('/').at(-1) === 'menu.html') {
-        coffeeMenuLink.classList.toggle('coffee-menu__link_current');
+    if (currentUrl.split("/").at(-1) === "menu.html") {
+        coffeeMenuLink.classList.toggle("coffee-menu__link_current");
     }
-};
+}
 
 function switchTabs(cupInd = 0) {
     tabInd = cupInd;
     cupsCards.forEach((item, ind) => {
         if (ind === tabInd) {
-            item.style.display = 'flex';
-            tabsBtn[ind].classList.add('active');
+            item.style.display = "flex";
+            tabsBtn[ind].classList.add("active");
         } else {
-            item.style.display = 'none';
-            tabsBtn[ind].classList.remove('active');
+            item.style.display = "none";
+            tabsBtn[ind].classList.remove("active");
         }
-    })
+    });
 }
 
 function closeModal() {
-    modalWindow.style.display = 'none';
-    overlay.style.display = 'none';
-    page.style.overflow = 'inherit';
+    modalWindow.style.display = "none";
+    overlay.style.display = "none";
+    page.style.overflow = "inherit";
 }
 
 function openModal() {
-    window.scrollTo(0, 0)
-    modalWindow.style.display = 'flex';
-    overlay.style.display = 'table';
-    page.style.overflow = 'hidden';
-
+    window.scrollTo(0, 0);
+    modalWindow.style.display = "flex";
+    overlay.style.display = "table";
+    page.style.overflow = "hidden";
 }
 
-showUnderline()
-switchTabs()
+showUnderline();
+switchTabs();
+
+modalWindow.innerHTML = `
+            <div class="modal__image-box">
+                <img alt="product image" class="modal__image" src="">
+            </div>
+
+            <div class="modal__content">
+                <div class="modal__text">
+                    <h2 class="modal__title"></h2>
+                    <p class="modal__description"></p>
+                </div>
+
+                <div class="modal__size size">
+                    <h3 class="size__title">Size</h3>
+                    <div class="size__btn-box">
+                        <button class="size__btn tabs__button">
+                            <span class="size__btn-icon button__icon">S</span>
+                            <span class="button__name"></span>
+                        </button>
+                        <button class="size__btn tabs__button"><span class="size__btn-icon button__icon">M</span>
+                            <span class="button__name"></span>
+                        </button>
+                        <button class="size__btn tabs__button"><span class="size__btn-icon button__icon">L</span>
+                            <span class="button__name"></span>
+                        </button>
+                    </div>
+
+                </div>
+
+                <div class="modal__additives additives">
+                    <h3 class="additives__title">Additives</h3>
+                    <div class="additives__btn-box">
+                        <button class="additives__btn tabs__button">
+                            <span class="additives__btn-icon button__icon">1</span>
+                            <span class="button__name"></span>
+                        </button>
+                        <button class="additives__btn tabs__button">
+                            <span class="additives__btn-icon button__icon">2</span>
+                            <span class="button__name"></span>
+                        </button>
+                        <button class="additives__btn tabs__button">
+                            <span class="additives__btn-icon button__icon">3</span>
+                            <span class="button__name"></span>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="modal__cost cost">
+                    <span class="cost__title">Total:</span>
+                    <span class="cost__value"></span>
+                </div>
+
+                <div class="modal__note">
+                    <img alt="info-empty" class="note__icon" src="pictures/menu/info-empty.svg">
+                    <p class="note__text">The cost is not final. Download our mobile app to see the final price and
+                        place your order. Earn loyalty points and enjoy your favorite coffee with up to 20%
+                        discount.
+                    </p>
+                </div>
+
+                <div class="modal__close">
+                    <button class="close__btn">Close</button>
+                </div>
+            </div>
+`;
+
+async function showProducts(section = "coffee") {
+    let response = await fetch(jsonURL);
+    products = await response.json(); // читаем ответ в формате JSON
+    console.log(products);
+    let startInd = rangeSections[section].start;
+    let endInd = rangeSections[section].end;
+    let menuSectionEl = document.querySelector(`.menu__${section}`);
+    for (let i = startInd; i <= endInd; i++) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("menu__cup-card");
+        productCard.innerHTML = `
+        <div class="menu__cup-card">
+             <div class="card__image">
+                <img alt="${products[i].category}-${i+1}" src="images/menu/${products[0].category}-${i+1}.png">
+             </div>
+            <div class="cup-card__info">
+                <div><h2 class="cup-card__name"></h2>${products[i].name}</div>
+                <div>
+                    <p class="cup-card__description">${products[i].description}</p>
+                </div>
+                <div><span class="cup-card__cost">$${products[i].price}</span></div>
+            </div>
+        </div>
+        `;
+
+        productCard.addEventListener("click", () => openModal(i));
+        menuSectionEl.appendChild(productCard);
+    }
+}
+
+showProducts();
+
+class MenuCard {
+    constructor(props) { }
+}
